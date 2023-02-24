d489f0524e14bcc1ff17ae2cf441a663be236450
Prithviraj Awatade <96919987+prithvi009@users.noreply.github.com>
2023-02-24 21:23:03 +0530

    started development

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     hello prithviraj 
+    </div>
+  );
+}
+
+export default App;
---
+++ server/contracts/RealEstateToken.sol
@@ -0,0 +1,64 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract RealEstateToken {
+    
+    struct RealEstate {
+        address owner;
+        string addressOfLand;
+        // string[] images;
+        string ownerName;
+        uint purchaseDate;
+        bool isForSale;
+        uint salePrice;
+    }
+    
+    RealEstate[] public realEstates;
+    
+    mapping(uint => address) public realEstateToOwner;
+    mapping(address => uint) public ownerRealEstateCount;
+    
+    event NewRealEstate(uint realEstateId, string tokenAddress);
+    event RealEstateOwnershipTransferred(uint realEstateId, address previousOwner, address newOwner);
+    event RealEstateSetForSale(uint realEstateId, uint salePrice);
+    event RealEstateSold(uint realEstateId, address buyer, uint salePrice);
+    
+    function createRealEstate(string memory _address, string memory _ownerName, uint _purchaseDate) public {
+        RealEstate memory newRealEstate = RealEstate({
+            owner: msg.sender,
+            addressOfLand: _address,
+            // images: _images,
+            ownerName: _ownerName,
+            purchaseDate: _purchaseDate,
+            isForSale: false,
+            salePrice: 0
+        });
+        realEstates.push(newRealEstate);
+        uint id = realEstates.length +1;
+        realEstateToOwner[id] = msg.sender;
+        ownerRealEstateCount[msg.sender]++;
+        emit NewRealEstate(id, _address);
+    }
+    
+    function setRealEstateForSale(uint _realEstateId, uint _salePrice) public {
+        require(realEstates[_realEstateId].owner == msg.sender, "You are not the owner of this real estate");
+        realEstates[_realEstateId].isForSale = true;
+        realEstates[_realEstateId].salePrice = _salePrice;
+        emit RealEstateSetForSale(_realEstateId, _salePrice);
+    }
+    
+    function buyRealEstate(uint _realEstateId) public payable {
+        require(realEstates[_realEstateId].isForSale == true, "This real estate is not for sale");
+        require(msg.value == realEstates[_realEstateId].salePrice, "The amount of Ether sent is not equal to the sale price");
+        address payable previousOwner = payable(realEstates[_realEstateId].owner);
+        address payable newOwner = payable(msg.sender);
+        previousOwner.transfer(msg.value);
+        realEstateToOwner[_realEstateId] = newOwner;
+        ownerRealEstateCount[previousOwner]--;
+        ownerRealEstateCount[newOwner]++;
+        realEstates[_realEstateId].isForSale = false;
+        realEstates[_realEstateId].salePrice = 0;
+        emit RealEstateSold(_realEstateId, newOwner, msg.value);
+        emit RealEstateOwnershipTransferred(_realEstateId, previousOwner, newOwner);
+    }
+}
